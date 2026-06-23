6eb31eec2d516aa9009cd46832b224d383db157b
ItakawaM <maksymworkp@gmail.com>
2026-06-23 22:56:55 +0300

    feat(api): update handlers to send json responses

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,6 +5,10 @@ import "net/http"
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		app.notFoundResponse(w, r)
+	})
+
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
