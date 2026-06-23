77893f34edebcc1ad29cf3c1f24483d96d8ba297
ItakawaM <maksymworkp@gmail.com>
2026-06-23 17:50:32 +0300

    feat(api): scaffold /movies routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,5 +7,8 @@ func (app *application) routes() http.Handler {
 
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 
+	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
+
 	return mux
 }
