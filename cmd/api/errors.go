a504b1f2c183d349ccfbd4c0e80a1f77c58bff85
ItakawaM <maksymworkp@gmail.com>
2026-06-24 15:31:08 +0300

    feat(api): update createMovieHandler to successfully parse and validate incoming movie data

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -28,3 +28,11 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
