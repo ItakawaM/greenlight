cdaeef7dea872f299e29b16608e1350c9fba0ef4
ItakawaM <maksymworkp@gmail.com>
2026-07-18 14:22:23 +0300

    feat(api): define panic recovering middleware

---
+++ cmd/api/middleware.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// recoverPanic is a middleware that gracefully recovers panics in http method handlers,
+// logs them and sends a 500 response to the client.
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
