96fb7ea7de13a3dac109906a9ada5ae843a0e8b6
ItakawaM <maksymworkp@gmail.com>
2026-07-24 21:16:00 +0300

    fix(api): improved graceful shutdown by removing a potential goroutine leak

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -25,28 +25,40 @@ func (app *application) serve() error {
 	}
 
 	shutdownErr := make(chan error)
+	quit := make(chan os.Signal, 1)
+	done := make(chan struct{})
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		quit := make(chan os.Signal, 1)
+		defer signal.Stop(quit)
 
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		s := <-quit
+		select {
+		case s := <-quit:
+			app.logger.Info("shutting down server",
+				slog.String("signal", s.String()))
 
-		app.logger.Info("shutting down server",
-			slog.String("signal", s.String()))
+			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+			defer cancel()
 
-		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-		defer cancel()
+			shutdownErr <- srv.Shutdown(ctx)
 
-		shutdownErr <- srv.Shutdown(ctx)
+		case <-done:
+			// server already stopped on its own
+			// nothing to shut down, exit without sending on shutdownErr
+		}
 	}()
 
 	app.logger.Info("starting server",
 		slog.String("addr", srv.Addr),
 		slog.String("env", app.config.environment))
 
+	// we have to check if ListenAndServe itself was successful
+	err := srv.ListenAndServe()
+	close(done)
+
 	// if ListenAndServe returns a http.ErrServerClosed it means that
 	// the graceful shutdown has started
-	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+	if !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
 
