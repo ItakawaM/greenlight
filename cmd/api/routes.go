086df1026be954a3cfc4d8ce365aa61d43841ba5
ItakawaM <maksymworkp@gmail.com>
2026-07-25 23:28:18 +0300

    feat(api): define POST /users endpoint that registers a user

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,11 +12,15 @@ func (app *application) routes() http.Handler {
 
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 
+	// Movies API
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("GET /v1/movies", app.listMoviesHandler)
 	mux.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
 	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
+
+	// Users API
+	mux.HandleFunc("POST /v1/users", app.registerUserHandler)
 
 	return app.recoverPanic(app.rateLimit(mux))
 }
---
+++ cmd/api/users.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/ItakawaM/greenlight/internal/data"
+	"github.com/ItakawaM/greenlight/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := &data.User{
+		Name:     input.Name,
+		Email:    input.Email,
+		IsActive: false,
+	}
+
+	if err := user.Password.Set(input.Password); err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
+	defer cancel()
+
+	if err := app.models.Users.Insert(ctx, user); err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+
+		default:
+			app.handleModelError(w, r, err)
+		}
+		return
+	}
+
+	if err := app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
