929a536a8eb623883102056d1a2d3cf6d83e0f75
ItakawaM <maksymworkp@gmail.com>
2026-08-02 13:42:15 +0300

    fix(api): replace consecutive db calls via models with transactions to enforce integrity and rollback on db fail

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -44,7 +44,23 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
 	defer cancel()
 
-	if err := app.models.Users.Insert(ctx, user); err != nil {
+	// we run user creation and token assignment in a transaction
+	// to avoid corrupted states if one of the operations fail
+	// and allow for rollback
+	var token *data.Token
+	if err := app.models.WithTx(ctx, func(m *data.Models) error {
+		var err error
+		if err := m.Users.Insert(ctx, user); err != nil {
+			return err
+		}
+
+		token, err = m.Tokens.New(ctx, user.ID, 72*time.Hour, data.ScopeActivation)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
 		switch {
 		case errors.Is(err, data.ErrDuplicateEmail):
 			v.AddError("email", "a user with this email address already exists")
@@ -53,12 +69,6 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		default:
 			app.handleModelError(w, r, err)
 		}
-		return
-	}
-
-	token, err := app.models.Tokens.New(ctx, user.ID, 72*time.Hour, data.ScopeActivation)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
 		return
 	}
 
@@ -99,8 +109,28 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
 
-	user, err := app.models.Users.GetForToken(ctx, data.ScopeActivation, input.TokenPlaintext)
-	if err != nil {
+	// we run the following operations in a transaction
+	// to avoid corrupted states if one of the operations fail
+	// and allow for rollback
+	var user *data.User
+	if err := app.models.WithTx(ctx, func(m *data.Models) error {
+		var err error
+		user, err = m.Users.GetForToken(ctx, data.ScopeActivation, input.TokenPlaintext)
+		if err != nil {
+			return err
+		}
+
+		user.IsActive = true
+		if err := m.Users.Update(ctx, user); err != nil {
+			return err
+		}
+
+		if err := m.Tokens.DeleteAllForUser(ctx, user.ID, data.ScopeActivation); err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
 			v.AddError("token", "invalid or expired activation token")
@@ -109,17 +139,6 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 		default:
 			app.handleModelError(w, r, err)
 		}
-		return
-	}
-
-	user.IsActive = true
-	if err := app.models.Users.Update(ctx, user); err != nil {
-		app.handleModelError(w, r, err)
-		return
-	}
-
-	if err := app.models.Tokens.DeleteAllForUser(ctx, user.ID, data.ScopeActivation); err != nil {
-		app.handleModelError(w, r, err)
 		return
 	}
 
