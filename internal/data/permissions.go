9433b65385dde116c3b23b123b81d0bd804719f8
ItakawaM <maksymworkp@gmail.com>
2026-08-09 14:17:45 +0300

    docs(data): add documentation to permissions model

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -5,20 +5,26 @@ import (
 	"slices"
 )
 
+// Permissions represent user permission records in the application.
 type Permissions []string
 
+// Include checks whether permissions contain code.
 func (p *Permissions) Include(code string) bool {
 	return slices.Contains(*p, code)
 }
 
+// PermissionModelInterface defines the storage operations available for permissions.
 type PermissionModelInterface interface {
+	// GetAllForUser retrieves all permission codes for a specified user.
 	GetAllForUser(ctx context.Context, id int64) (Permissions, error)
 }
 
+// PermissionModel implements PermissionModelInterface.
 type PermissionModel struct {
 	db DBTX
 }
 
+// GetAllForUser implements PermissionModelInterface.
 func (m *PermissionModel) GetAllForUser(ctx context.Context, id int64) (Permissions, error) {
 	statement :=
 		`SELECT permissions.code
