7b099fd76e157523824ff6a532ceaf0c761de1b2
ItakawaM <maksymworkp@gmail.com>
2026-08-25 21:25:14 +0300

    feat(docker): add exporter to docker-compose; fix(docker): fix mktemp syntax in redis entrypoint

--- docker/redis/entrypoint.sh
+++ docker/redis/entrypoint.sh
@@ -7,7 +7,9 @@ if [ -z "$REDIS_PASSWORD" ]; then
   exit 1
 fi
 
-REDIS_CONF="$(mktemp /usr/local/etc/redis/redis.XXXXXX.conf)"
+REDIS_CONF="$(mktemp /usr/local/etc/redis/redis.XXXXXX)"
 ESCAPED_PASSWORD=$(printf '%s' "$REDIS_PASSWORD" | sed 's/[\/&\\]/\\&/g')
 sed "s/\${REDIS_PASSWORD}/$ESCAPED_PASSWORD/g" /usr/local/etc/redis/redis.conf.template > "$REDIS_CONF"
+chmod 600 "$REDIS_CONF"
+
 exec redis-server "$REDIS_CONF"
